9f08ae0040955be7e8813c57b5e9a87248a42e7f
shadiqurshaon <shadiqurshaon@gmail.com>
2018-12-26 00:06:23 +0600

    working for single keyword it can be use testing perpus

--- crawl.py
+++ crawl.py
@@ -1,20 +1,33 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+news = [];
+
+# Request from url
 url = 'https://www.prothomalo.com'
-keyword = '/bangladesh'
+keyword = '/international'
 doc = requests.get(url+keyword)
 doctext = doc.text
-
+#make parser object
 soup = BeautifulSoup(doctext, 'html.parser')
-
+#find all links
 for link in soup.findAll('a'):
-  m=re.match("(^/bangladesh/.+[^(#comments)]$)",link.get('href'))
+  m=re.match("(^/international/article/.+[^(#comments)]$)",link.get('href'))
   if m:
     full_url = url+link.get('href')
     link_object = requests.get(full_url)
     soup = BeautifulSoup(link_object.text, 'html.parser')
-    des = soup.find("meta",  property='og:description')
-    print(des.get('content'))
-    break
+    news_object = {};
+    # get data for every url
+    title = soup.find("meta",  property='og:title')
+    description = soup.find("meta",  property='og:description')
+    site_name = soup.find("meta",  property='og:site_name')
+    image = soup.find("meta",  property='og:image')
+    #make object of a link
+    news_object['title'] =  title.get('content')
+    news_object['description'] =  description.get('content')
+    news_object['site_name'] =  site_name.get('content')
+    news_object['image'] =  image.get('content')
+    news.append(news_object)
 
+print(news)
